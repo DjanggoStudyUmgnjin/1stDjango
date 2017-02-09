73ca5784a2f2ef33ea7debc8161664ec50d05cdd
jomno <jake8955@naver.com>
2017-02-09 13:30:14 +0900

    2장 끝

--- blog/models.py
+++ blog/models.py
@@ -2,34 +2,34 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from taggit.managers import TaggableManager
 
 class PublishedManager(models.Manager):
-        def get_queryset(self):
-            return super(PublishedManager,
-                self).get_queryset()\
-                    .filter(status='published')
+    def get_queryset(self):
+        return super(PublishedManager,
+                     self).get_queryset()\
+                            .filter(status="published")
 
-class Post(models.Model):
+class Post(models.Model) :
     STATUS_CHOICES = (
         ('draft', 'Draft'),
-        ('published', 'Published'),
+        ('published', 'Published')
     )
-
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250,
-    unique_for_date='publish')
+                            unique_for_date='publish')
     author = models.ForeignKey(User,
-    related_name='blog_posts')
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
     status = models.CharField(max_length=10,
-                            choices=STATUS_CHOICES,
-                            default='draft')
-
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
+                              choices=STATUS_CHOICES,
+                              default='draft')
+    objects = models.Manager()
+    published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ('-publish',)
@@ -44,4 +44,18 @@ class Post(models.Model):
                                 self.publish.strftime('%d'),
                                 self.slug])
 
-# Create your models here.
+class Comment(models.Model):
+    post = models.ForeignKey(Post, related_name='comments')
+
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+
+    def __str__(self):
+        return 'Comment by {} on {}'.format(self.name, self.post)
--- mysite/settings.py
+++ mysite/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'taggit',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -101,3 +102,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'jake89577@gmail.com'
+EMAIL_HOST_PASSWORD = 'jake5355'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
